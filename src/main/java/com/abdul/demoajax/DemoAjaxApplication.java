dfec1b05553ba8f96ebfcd4de22f4e69dd42891e
Momade Abdul <momademha@gmail.com>
2019-09-12 08:52:18 +0200

    Ajustes finais na classe de servico

---
+++ src/main/java/com/abdul/demoajax/DemoAjaxApplication.java
@@ -0,0 +1,30 @@
+package com.abdul.demoajax;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.abdul.demoajax.domain.SocialMetaTag;
+import com.abdul.demoajax.service.SocialMetaTagService;
+
+@SpringBootApplication
+public class DemoAjaxApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAjaxApplication.class, args);
+	}
+
+	
+	@Autowired
+	SocialMetaTagService service;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		SocialMetaTag tag = service.getSocialMetaTagByUrl("https://www.pichau.com.br/gabinete-gamer-azza-luminous-110-lateral-de-vidro-led-rgb-azza-110");
+		System.out.println(tag.toString());
+		
+	}
+
+}
