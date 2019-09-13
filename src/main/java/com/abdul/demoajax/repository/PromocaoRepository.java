37ae234abad9510341928fecaec754e2eb234a41
Momade Abdul <momademha@gmail.com>
2019-09-13 13:21:21 +0200

    Na sala de Aulas UJC

---
+++ src/main/java/com/abdul/demoajax/repository/PromocaoRepository.java
@@ -0,0 +1,9 @@
+package com.abdul.demoajax.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.abdul.demoajax.domain.Promocao;
+
+public interface PromocaoRepository extends JpaRepository<Promocao, Long> {
+
+}
