ae8c362cbeee1a38825dd5c2cc804a34244309ab
Momade Abdul <momademha@gmail.com>
2019-09-20 17:02:45 +0200

    Versao quase finalisada. Falta apenas um pequeno ajuste para que O DWR funcione correctamente de tal forma que caso seja registada uma nova Promacao os outros utilizadores activos possam receber alertas em tempo real

--- src/main/java/com/abdul/demoajax/DemoAjaxApplication.java
+++ src/main/java/com/abdul/demoajax/DemoAjaxApplication.java
@@ -1,9 +1,12 @@
 package com.abdul.demoajax;
 
+import org.directwebremoting.spring.DwrSpringServlet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 import com.abdul.demoajax.domain.SocialMetaTag;
 import com.abdul.demoajax.service.SocialMetaTagService;
@@ -15,10 +18,9 @@ public class DemoAjaxApplication implements CommandLineRunner {
 		SpringApplication.run(DemoAjaxApplication.class, args);
 	}
 
-	
 	@Autowired
 	SocialMetaTagService service;
-	
+
 	@Override
 	public void run(String... args) throws Exception {
 		/*
@@ -26,7 +28,19 @@ public class DemoAjaxApplication implements CommandLineRunner {
 		 * "https://www.pichau.com.br/gabinete-gamer-azza-luminous-110-lateral-de-vidro-led-rgb-azza-110"
 		 * ); System.out.println(tag.toString());
 		 */
-		
+
+	}
+
+	@Bean
+	public ServletRegistrationBean<DwrSpringServlet> dwrSpringServlet() {
+		DwrSpringServlet dwrServlet = new DwrSpringServlet();
+
+		ServletRegistrationBean<DwrSpringServlet> registrationBean = new ServletRegistrationBean<>(dwrServlet,
+				"/dwr/*");
+
+		registrationBean.addInitParameter("debug", "true");
+		registrationBean.addInitParameter("activeReverseAjaxEnabled", "true");
+		return registrationBean;
 	}
 
 }
